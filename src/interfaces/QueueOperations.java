66c0f7c50e9beee696857d7bd5f421ef59893a7a
BoboNikolov <x24109479@student.ncirl.ie>
2026-03-18 23:42:33 +0000

    Add comment to QueueOperations interface
    Added a comment to describe the purpose of the QueueOperations interface.

--- src/interfaces/QueueOperations.java
+++ src/interfaces/QueueOperations.java
@@ -2,6 +2,7 @@ package interfaces;
 
 import java.util.List;
 
+// Generic queue interface, used for the issue of priority queue
 public interface QueueOperations<T> {
     void enqueue(T item);
     T dequeue();
